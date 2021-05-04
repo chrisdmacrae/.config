a4672e65c4e00be8e918bd1adb3a364135bd41b5
Chris D. Macrae <chris.macrae@torqit.ca>
2021-05-03 21:08:45 -0300

    Add Windows install script

---
+++ install.ps1
@@ -0,0 +1,99 @@
+# Install script for Windows-based systems
+
+## Script Inputs and Logic
+$APP_NAMES = @(
+    'git',
+    'gh',
+    'github-desktop',
+    'visualstudiocode',
+    'jetbrains-rider',
+    'visualstudiocode',
+    'dotnetcore-sdk',
+    'nodejs',
+    'ruby',
+    'slack',
+    'microsoft-windows-terminal',
+    'spotify'
+)
+
+function Prompt-YesNo($title, $message) {
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Description."
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","Description."
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    $result = $host.ui.PromptForChoice($title, $message, $options, 1)
+
+    return $result
+}
+
+function InstallApps($appNames, $prompt) {
+    $appNames | ForEach-Object {
+        $shouldInstall = $true
+
+        if ($prompt) {
+            $result = Prompt-YesNo(
+                $_ + ":",
+                "would you like to install $_" + "?"
+            )
+
+            if ($result -eq 1) {
+                $shouldInstall = $false
+            }
+        }
+
+        if ($shouldInstall) {
+            InstallApp $_
+        }
+    }
+}
+
+Function InstallApp($appName) {
+    try {
+        choco feature enable -n allowGlobalConfirmation
+
+        choco install $appName
+
+        choco feature disable -n allowGlobalConfirmation
+    }
+    Catch {
+        Write-Output "Failed to install $appName"
+    }
+}
+
+Function Test-CommandExists($command) {
+ $exists = $false
+ $oldPreference = $ErrorActionPreference
+ $ErrorActionPreference = 'stop'
+
+ try {
+    if (Get-Command $command) {
+        $exists = $true
+    }
+  }
+  Catch {
+    $exists = $false
+  }
+
+  Finally {
+    $ErrorActionPreference = $oldPreference
+  }
+
+  return $exists
+}
+
+## Script Execution
+
+Write-Output "Welcome to .config"
+
+$HAS_CHOCOLATEY = Test-CommandExists 'choco'
+if (!$HAS_CHOCOLATEY) {
+  Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+  Set-Variable PATH="%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+}
+
+$shouldPrompt = Prompt-YesNo(
+    "Auto-install?",
+    "Would you like to auto-install or choose which applications and tools to install?"
+) -eq 1
+InstallApps $APP_NAMES $shouldPrompt
+
+Read-Host -Prompt "Press any key to exit"
